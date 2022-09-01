09528c3b1e5b62728c8de631dfc9085d75c1897f
valflrt <vfleurit@gmail.com>
2022-09-01 10:40:19 +0200

    Group basic crypto function into a struct
    - Edit tests

--- src/crypto.rs
+++ src/crypto.rs
@@ -8,47 +8,56 @@ use ring::digest;
 
 const NONCE_LEN: usize = 12;
 
-pub fn encrypt(key: &[u8], plain_data: &[u8]) -> anyhow::Result<Vec<u8>> {
-    let hashed_key = hash_key(key);
-    let nonce = Nonce::from(generate_nonce());
-
-    let cipher = match Aes256Gcm::new_from_slice(&hashed_key) {
-        Ok(v) => v,
-        Err(e) => return Err(anyhow!("Failed to create cipher: {}", e)),
-    };
-
-    let encrypted_data = match cipher.encrypt(&nonce, plain_data) {
-        Ok(enc) => enc,
-        Err(e) => return Err(anyhow!("Failed to encrypt: {}", e)),
-    };
-
-    Ok([nonce.to_vec(), encrypted_data].concat())
+pub struct Crypto {
+    hashed_key: Vec<u8>,
 }
 
-pub fn decrypt(key: &[u8], encrypted_data: &[u8]) -> anyhow::Result<Vec<u8>> {
-    let hashed_key = hash_key(key);
+impl Crypto {
+    pub fn new(key: Vec<u8>) -> Crypto {
+        Crypto {
+            hashed_key: Self::hash_key(key.as_slice()),
+        }
+    }
 
-    let nonce = Nonce::from_slice(&encrypted_data[..NONCE_LEN]);
+    pub fn encrypt(&self, plain_data: &[u8]) -> anyhow::Result<Vec<u8>> {
+        let nonce = Nonce::from(Self::generate_nonce());
 
-    let cipher = match Aes256Gcm::new_from_slice(&hashed_key) {
-        Ok(v) => v,
-        Err(e) => return Err(anyhow!("Failed to create cipher: {}", e)),
-    };
+        let cipher = match Aes256Gcm::new_from_slice(&self.hashed_key) {
+            Ok(v) => v,
+            Err(e) => return Err(anyhow!("Failed to create cipher: {}", e)),
+        };
 
-    let decrypted_data = match cipher.decrypt(nonce, &encrypted_data[NONCE_LEN..]) {
-        Ok(dec) => dec,
-        Err(e) => return Err(anyhow!("Failed to decrypt: {}", e)),
-    };
+        let encrypted_data = match cipher.encrypt(&nonce, plain_data) {
+            Ok(enc) => enc,
+            Err(e) => return Err(anyhow!("Failed to encrypt: {}", e)),
+        };
 
-    Ok(decrypted_data)
-}
+        Ok([nonce.to_vec(), encrypted_data].concat())
+    }
 
-pub fn hash_key(key: &[u8]) -> Vec<u8> {
-    digest::digest(&digest::SHA256, key).as_ref().to_owned()
-}
+    pub fn decrypt(&self, encrypted_data: &[u8]) -> anyhow::Result<Vec<u8>> {
+        let nonce = Nonce::from_slice(&encrypted_data[..NONCE_LEN]);
 
-fn generate_nonce() -> [u8; NONCE_LEN] {
-    let mut nonce = [0; NONCE_LEN];
-    OsRng.fill_bytes(&mut nonce);
-    nonce
+        let cipher = match Aes256Gcm::new_from_slice(&self.hashed_key) {
+            Ok(v) => v,
+            Err(e) => return Err(anyhow!("Failed to create cipher: {}", e)),
+        };
+
+        let decrypted_data = match cipher.decrypt(nonce, &encrypted_data[NONCE_LEN..]) {
+            Ok(dec) => dec,
+            Err(e) => return Err(anyhow!("Failed to decrypt: {}", e)),
+        };
+
+        Ok(decrypted_data)
+    }
+
+    pub fn hash_key(key: &[u8]) -> Vec<u8> {
+        digest::digest(&digest::SHA256, key).as_ref().to_owned()
+    }
+
+    fn generate_nonce() -> [u8; NONCE_LEN] {
+        let mut nonce = [0; NONCE_LEN];
+        OsRng.fill_bytes(&mut nonce);
+        nonce
+    }
 }
--- src/test.rs
+++ src/test.rs
@@ -1,13 +1,13 @@
-use crate::crypto;
+use crate::crypto::Crypto;
 
 const KEY: &str = "my_super_key";
 const PLAIN_DATA: &str = "hello :)";
 
 #[test]
 fn encrypt_data_without_error() {
-    let hashed_key = crypto::hash_key(KEY.as_bytes());
+    let crypto = Crypto::new(KEY.as_bytes().to_vec());
 
-    match crypto::encrypt(&hashed_key, PLAIN_DATA.as_bytes()) {
+    match crypto.encrypt(PLAIN_DATA.as_bytes()) {
         Ok(_) => (),
         Err(_) => panic!("Failed to encrypt"),
     };
@@ -15,14 +15,14 @@ fn encrypt_data_without_error() {
 
 #[test]
 fn decrypt_data_without_error() {
-    let hashed_key = crypto::hash_key(KEY.as_bytes());
+    let crypto = Crypto::new(KEY.as_bytes().to_vec());
 
-    let encrypted_data = match crypto::encrypt(&hashed_key, PLAIN_DATA.as_bytes()) {
+    let encrypted_data = match crypto.encrypt(PLAIN_DATA.as_bytes()) {
         Ok(enc) => enc,
         Err(_) => panic!("Failed to encrypt"),
     };
 
-    match crypto::decrypt(&hashed_key, &encrypted_data) {
+    match crypto.decrypt(&encrypted_data) {
         Ok(_) => (),
         Err(_) => panic!("Failed to decrypt"),
     };
@@ -30,14 +30,14 @@ fn decrypt_data_without_error() {
 
 #[test]
 fn can_encrypt_then_decrypt_and_get_original_data() {
-    let hashed_key = crypto::hash_key(KEY.as_bytes());
+    let crypto = Crypto::new(KEY.as_bytes().to_vec());
 
-    let encrypted_data = match crypto::encrypt(&hashed_key, PLAIN_DATA.as_bytes()) {
+    let encrypted_data = match crypto.encrypt(PLAIN_DATA.as_bytes()) {
         Ok(enc) => enc,
         Err(_) => panic!("Failed to encrypt"),
     };
 
-    let new_plain_data = match crypto::decrypt(&hashed_key, &encrypted_data) {
+    let new_plain_data = match crypto.decrypt(&encrypted_data) {
         Ok(dec) => dec,
         Err(_) => panic!("Failed to decrypt"),
     };
