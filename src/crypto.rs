77701cd2d3b8fe6e89d03e26758ae36370c1e836
valflrt <vfleurit@gmail.com>
2022-09-03 16:57:04 +0200

    Switch from aes-gcm to chacha20poly1305

--- src/crypto.rs
+++ src/crypto.rs
@@ -1,25 +1,21 @@
-use aes_gcm::{aead::OsRng, aes::cipher::InvalidLength, AeadInPlace, Aes256Gcm, KeyInit, Nonce};
-use anyhow::anyhow;
-use rand::RngCore;
+use chacha20poly1305::{aead::Aead, KeyInit, XChaCha20Poly1305};
+use rand::{rngs::OsRng, RngCore};
 use ring::digest;
 
-const NONCE_LEN: usize = 96 / 8;
-const TAG_LEN: usize = 128 / 8;
+const NONCE_LEN: usize = 24;
+// const TAG_LEN: usize = 128 / 8;
 
 pub struct Crypto {
-    cipher: Aes256Gcm,
+    cipher: XChaCha20Poly1305,
 }
 
 impl Crypto {
     /// Creates a new Crypto instance, the given key will be
     /// used for every operation performed.
-    pub fn new(key: &[u8]) -> Result<Crypto, InvalidLength> {
-        let cipher = match Aes256Gcm::new_from_slice(&hash_key(key)) {
-            Ok(v) => v,
-            Err(e) => return Err(e),
-        };
-
-        Ok(Crypto { cipher })
+    pub fn new(key: &[u8]) -> Crypto {
+        Crypto {
+            cipher: XChaCha20Poly1305::new(hash_key(key)[..].into()),
+        }
     }
 
     /// Encrypt a small piece of data.
@@ -42,18 +38,13 @@ impl Crypto {
     ///     Err(e) => panic!("Failed to encrypt: {}", e),
     /// };
     /// ```
-    pub fn encrypt(&self, plain_data: &[u8]) -> anyhow::Result<Vec<u8>> {
-        let nonce = Nonce::from(randomize_nonce());
-        let mut buffer = Vec::with_capacity(plain_data.len() + TAG_LEN);
-        buffer.extend_from_slice(plain_data);
+    pub fn encrypt(&self, plain_data: &[u8]) -> Result<Vec<u8>, chacha20poly1305::aead::Error> {
+        let nonce = random_nonce();
 
-        if let Err(e) = self.cipher.encrypt_in_place(&nonce, &[], &mut buffer) {
-            return Err(anyhow!(e));
-        };
-
-        let result = [nonce.to_vec(), buffer].concat();
-
-        Ok(result)
+        match self.cipher.encrypt(nonce[..].into(), plain_data) {
+            Ok(v) => Ok([nonce.to_vec(), v].concat()),
+            Err(e) => return Err(e),
+        }
     }
 
     /// Decrypt a small encrypted piece of data.
@@ -80,15 +71,14 @@ impl Crypto {
     ///     Err(e) => panic!("Failed to decrypt: {}", e),
     /// };
     /// ```
-    pub fn decrypt(&self, encrypted_data: &[u8]) -> anyhow::Result<Vec<u8>> {
-        let nonce = Nonce::from_slice(&encrypted_data[..NONCE_LEN]);
-        let mut buffer = encrypted_data[NONCE_LEN..].to_vec();
+    pub fn decrypt(&self, encrypted_data: &[u8]) -> Result<Vec<u8>, chacha20poly1305::aead::Error> {
+        let nonce = &encrypted_data[..NONCE_LEN];
+        let ciphertext = &encrypted_data[NONCE_LEN..];
 
-        if let Err(e) = self.cipher.decrypt_in_place(nonce, &[], &mut buffer) {
-            return Err(anyhow!(e));
-        };
-
-        Ok(buffer)
+        match self.cipher.decrypt(nonce.into(), ciphertext) {
+            Ok(v) => Ok(v),
+            Err(e) => Err(e),
+        }
     }
 }
 
@@ -96,8 +86,8 @@ fn hash_key(key: &[u8]) -> Vec<u8> {
     digest::digest(&digest::SHA256, key).as_ref().to_owned()
 }
 
-fn randomize_nonce() -> [u8; NONCE_LEN] {
+fn random_nonce() -> Vec<u8> {
     let mut nonce = [0; NONCE_LEN];
     OsRng.fill_bytes(&mut nonce);
-    nonce
+    nonce.to_vec()
 }
