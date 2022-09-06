09ed86af80a0ab455dc9d7e30c721e2f9b2df3f1
valflrt <vfleurit@gmail.com>
2022-09-06 19:21:21 +0200

    Move cli handling from main to mod cli

---
+++ src/cli.rs
@@ -0,0 +1,30 @@
+use clap::Parser;
+
+use crate::commands;
+
+#[derive(Parser)]
+#[clap(name = "fencryption", version)]
+/// A program to encrypt/decrypt files and full directories
+struct Cli {
+    #[clap(subcommand)]
+    command: commands::Commands,
+
+    /// Enables debug log
+    #[clap(
+        short = 'D',
+        long,
+        value_parser,
+        default_value_t = false,
+        global = true
+    )]
+    debug: bool,
+}
+
+pub fn parse() {
+    match &Cli::parse().command {
+        commands::Commands::Encrypt(args) => commands::encrypt::action(args),
+        commands::Commands::EncryptStream(args) => commands::encrypt_stream::action(args),
+        commands::Commands::Decrypt(args) => commands::decrypt::action(args),
+        commands::Commands::DecryptStream(args) => commands::decrypt_stream::action(args),
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,8 @@
-use clap::Parser;
-
+mod cli;
 mod commands;
-
-#[derive(Parser)]
-#[clap(name = "fencryption", version)]
-/// A program to encrypt/decrypt files and full directories
-struct Cli {
-    #[clap(subcommand)]
-    command: commands::Commands,
-
-    /// Enables debug log
-    #[clap(
-        short = 'D',
-        long,
-        value_parser,
-        default_value_t = false,
-        global = true
-    )]
-    debug: bool,
-}
+mod crypto;
+mod tree;
 
 fn main() {
-    let cli = Cli::parse();
-
-    match &cli.command {
-        commands::Commands::Encrypt(args) => commands::encrypt::action(args),
-        commands::Commands::Decrypt(args) => commands::decrypt::action(args),
-    }
+    cli::parse();
 }
