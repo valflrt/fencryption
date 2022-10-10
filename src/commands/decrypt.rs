abe3415742093cc94fb60ff8219aa7fd1c08f0a3
valflrt <valflrt@pm.me>
2022-10-10 19:34:42 +0200

    Update commands encrypt and decrypt
    - Input path argument can now accept several paths
    - Add condition for setting output path (only one input path provided)

--- src/commands/decrypt.rs
+++ src/commands/decrypt.rs
@@ -17,11 +17,13 @@ pub struct Command {
     #[clap(value_parser)]
     key: String,
 
-    /// Path of the encrypted file/directory to decrypt
+    /// Paths of the encrypted file(s)/directory(ies) to
+    /// decrypt
     #[clap(value_parser)]
-    path: String,
+    paths: Vec<String>,
 
-    /// Set output path
+    /// Set output path (only supported when one input path
+    /// provided)
     #[clap(short, long, value_parser)]
     output_path: Option<String>,
 
@@ -34,59 +36,87 @@ pub fn action(args: &Command) {
 
     let crypto = Crypto::new(args.key.as_bytes());
 
-    let input_dir_path = PathBuf::from(&args.path);
-    let output_dir_path = match &args.output_path {
-        Some(v) => PathBuf::from(v),
-        None => {
-            let mut path = PathBuf::from(&args.path);
-            path.set_extension("dec");
-            path
-        }
-    };
+    // Runs for every provided input path
+    for input_path in &args.paths {
+        let input_path = PathBuf::from(input_path);
+        let output_path = match &args.output_path {
+            Some(v) => {
+                if &args.paths.len() == &1usize {
+                    PathBuf::from(v)
+                } else {
+                    panic!("Only one input path can be provided when setting an output path");
+                }
+            }
+            None => {
+                let mut path = PathBuf::from(&input_path);
+                path.set_extension("dec");
+                path
+            }
+        };
 
-    match fs::create_dir(&output_dir_path) {
-        Ok(_) => println!("created base directory: \"{}\"", &output_dir_path.display()),
-        Err(e) => match e.kind() {
-            io::ErrorKind::AlreadyExists => (),
-            e => panic!("Failed to create directory: {}", e),
-        },
-    }
+        // Reads entry metadata to act in consequence
+        let entry_metadata = fs::metadata(&input_path).expect("Failed to read entry metadata");
+        if entry_metadata.file_type().is_dir() {
+            // The case where the entry is a directory
 
-    let walk_dir = WalkDir::new(&input_dir_path).expect("Error: Failed to read directory.");
-
-    for entry in walk_dir {
-        let entry = entry.expect("Failed to read entry");
-
-        let entry_path = entry.path();
-
-        let new_entry_path = output_dir_path.join(
-            entry_path
-                .strip_prefix(&input_dir_path)
-                .expect("Failed to establish relative file path"),
-        );
-
-        let entry_type = entry.file_type().expect("Failed to read file type");
-
-        if entry_type.is_dir() {
-            match fs::create_dir(&new_entry_path) {
-                Ok(_) => println!("Created sub-directory: \"{}\"", &new_entry_path.display()),
-                Err(e) => match e.kind() {
+            // Creates base directory to put encrypted files
+            // in
+            if let Err(e) = fs::create_dir(&output_path) {
+                match e.kind() {
                     io::ErrorKind::AlreadyExists => (),
                     e => panic!("Failed to create directory: {}", e),
-                },
-            }
-        } else if entry_type.is_file() {
-            print!("{} ... ", &entry_path.display());
+                };
+            };
 
-            let mut source = File::open(&entry_path).expect("Failed to read source file");
-            let mut dest =
-                File::create(&new_entry_path).expect("Failed to create destination file");
+            let walk_dir = WalkDir::new(&input_path).expect("Failed to read directory");
+
+            // Runs for every entry in the specified directory
+            for entry in walk_dir {
+                let entry = entry.expect("Failed to read entry");
+                let entry_path = entry.path();
+                let new_entry_path = output_path.join(
+                    entry_path
+                        .strip_prefix(&input_path)
+                        .expect("Failed to establish relative file path"),
+                );
+
+                let entry_type = entry.file_type().expect("Failed to read file type");
+                if entry_type.is_dir() {
+                    if let Err(e) = fs::create_dir(&new_entry_path) {
+                        match e.kind() {
+                            io::ErrorKind::AlreadyExists => (),
+                            e => panic!("Failed to create directory: {}", e),
+                        };
+                    };
+                } else if entry_type.is_file() {
+                    print!("{} ... ", &entry_path.display());
+
+                    let mut source = File::open(&entry_path).expect("Failed to read source file");
+                    let mut dest =
+                        File::create(&new_entry_path).expect("Failed to create destination file");
+
+                    match crypto.decrypt_stream(&mut source, &mut dest) {
+                        Ok(_) => println!("Ok"),
+                        Err(e) => panic!("Failed to decrypt: {}", e),
+                    };
+                } else {
+                    println!("Skipped entry: Unknown type");
+                };
+            }
+        } else if entry_metadata.file_type().is_file() {
+            // The case where the entry is a file
+
+            print!("{} ... ", input_path.display());
+
+            let mut source = File::open(&input_path).expect("Failed to read source file");
+            let mut dest = File::create(&output_path).expect("Failed to create destination file");
 
             match crypto.decrypt_stream(&mut source, &mut dest) {
                 Ok(_) => println!("Ok"),
                 Err(e) => panic!("Failed to decrypt: {}", e),
             };
         } else {
+            // The case where the entry is something else
             println!("Skipped entry: Unknown type");
         }
     }
