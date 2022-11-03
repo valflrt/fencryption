7d142e3cee2134a4042c55e37c3c1bb6d2be9e3f
valflrt <valflrt@pm.me>
2022-11-03 18:42:58 +0100

    Big edit in mod pack
    - Make paths relative to the directory from which the pack was created
    - Remove structs PackWriter/Reader
    - Improve FileHeader

--- src/pack.rs
+++ src/pack.rs
@@ -1,18 +1,23 @@
 use std::{
-    fs::{self, File, OpenOptions},
+    fs::{self, OpenOptions},
     io::{self, Read, Write},
     path::PathBuf,
 };
 
-use crate::constants::DEFAULT_BUFFER_LEN;
+use crate::{
+    constants::DEFAULT_BUFFER_LEN,
+    walk_dir::{self, WalkDir},
+};
 
 const PATH_LEN_LEN: usize = 32 / 8; // 4 bytes
 const FILE_LEN_LEN: usize = 64 / 8; // 8 bytes
 const HEADER_LEN: usize = PATH_LEN_LEN + FILE_LEN_LEN; // 12 bytes
 
+// TODO Edit ErrorKind arms (they are ugly)
 #[derive(Debug)]
 pub enum ErrorKind {
     IO(io::Error),
+    WalkDir(walk_dir::ErrorKind),
     TryFromIntError,
     TryFromSliceError,
     Utf8Error,
@@ -21,147 +26,115 @@ pub enum ErrorKind {
     PathError,
 }
 
-type Result<T> = std::result::Result<T, ErrorKind>;
-
-// TODO make stored file paths relative to the pack path
+type Result<T, E = ErrorKind> = std::result::Result<T, E>;
 
 /// Pack is used to pack files and their paths in a single
 /// file
-pub struct Pack {
-    path: PathBuf,
-}
+pub struct Pack(PathBuf);
 
 impl Pack {
     /// Creates a new pack
-    pub fn new(path: PathBuf) -> Pack {
-        Pack { path }
+    pub fn new(path: &PathBuf) -> Pack {
+        Pack(path.to_path_buf())
     }
 
-    /// Creates a PackWriter, consumes self.
-    pub fn writer(self) -> Result<PackWriter> {
-        if self.path.exists() {
-            return Err(ErrorKind::PathAlreadyExists);
-        }
-        Ok(PackWriter::new(self)?)
-    }
-
-    /// Creates a PackReader, consumes self.
-    pub fn reader(self) -> Result<PackReader> {
-        if !self.path.exists() {
-            return Err(ErrorKind::PathNotFound);
-        }
-        Ok(PackReader::new(self)?)
-    }
-
-    pub fn path(&self) -> &PathBuf {
-        return &self.path;
-    }
-}
-
-pub struct PackWriter {
-    file: File,
-}
-
-impl PackWriter {
-    /// Creates a new PackWriter
-    pub fn new(pack: Pack) -> Result<PackWriter> {
-        Ok(PackWriter {
-            file: match OpenOptions::new().write(true).create(true).open(pack.path) {
-                Ok(v) => v,
-                Err(e) => return Err(ErrorKind::IO(e)),
-            },
-        })
-    }
-
-    /// Adds a file to the pack
-    pub fn add(&mut self, file_path: &str) -> Result<()> {
-        let mut file = match OpenOptions::new().read(true).open(file_path) {
+    pub fn create(&self, dir_path: &PathBuf) -> Result<()> {
+        let mut pack_file = match OpenOptions::new().write(true).create(true).open(&self.0) {
             Ok(v) => v,
             Err(e) => return Err(ErrorKind::IO(e)),
         };
 
-        // Creates file header.
-        let header = match FileHeader::new(file_path) {
+        let walk_dir = match WalkDir::new(&dir_path) {
             Ok(v) => v,
-            Err(e) => return Err(e),
+            Err(e) => return Err(ErrorKind::WalkDir(e)),
         };
 
-        // Writes file header to the pack.
-        if let Err(e) = self
-            .file
-            .write_all(&[&header.to_vec()?, file_path.as_bytes()].concat())
-        {
-            return Err(ErrorKind::IO(e));
-        };
+        for entry in walk_dir {
+            let entry = match entry {
+                Ok(v) => v,
+                Err(e) => return Err(ErrorKind::WalkDir(e)),
+            };
 
-        let mut buffer = [0u8; DEFAULT_BUFFER_LEN];
-        loop {
-            let read_len = match file.read(&mut buffer) {
+            if match entry.metadata() {
                 Ok(v) => v,
                 Err(e) => return Err(ErrorKind::IO(e)),
-            };
+            }
+            .is_file()
+            {
+                let mut file = match OpenOptions::new().read(true).open(entry.path()) {
+                    Ok(v) => v,
+                    Err(e) => return Err(ErrorKind::IO(e)),
+                };
 
-            if let Err(e) = self.file.write(&buffer[..read_len]) {
-                return Err(ErrorKind::IO(e));
-            };
+                // Creates file header.
+                let header = match FileHeader::new(&entry.path(), &dir_path) {
+                    Ok(v) => v,
+                    Err(e) => return Err(e),
+                };
 
-            if read_len != DEFAULT_BUFFER_LEN {
-                break;
+                // Writes file header to the pack.
+                if let Err(e) = pack_file.write_all(&header.to_vec()?) {
+                    return Err(ErrorKind::IO(e));
+                };
+
+                let mut buffer = [0u8; DEFAULT_BUFFER_LEN];
+                loop {
+                    let read_len = match file.read(&mut buffer) {
+                        Ok(v) => v,
+                        Err(e) => return Err(ErrorKind::IO(e)),
+                    };
+
+                    if let Err(e) = pack_file.write(&buffer[..read_len]) {
+                        return Err(ErrorKind::IO(e));
+                    };
+
+                    if read_len != DEFAULT_BUFFER_LEN {
+                        break;
+                    }
+                }
             }
         }
 
         Ok(())
     }
-}
 
-pub struct PackReader {
-    file: File,
-}
+    pub fn unpack(&self) -> Result<()> {
+        let mut pack_file = match OpenOptions::new().read(true).open(&self.0) {
+            Ok(v) => v,
+            Err(e) => return Err(ErrorKind::IO(e)),
+        };
 
-impl PackReader {
-    /// Creates a new PackReader
-    pub fn new(pack: Pack) -> Result<PackReader> {
-        Ok(PackReader {
-            file: match OpenOptions::new().read(true).open(pack.path) {
-                Ok(v) => v,
-                Err(e) => return Err(ErrorKind::IO(e)),
-            },
-        })
-    }
-
-    /// Write all the files to their associated path
-    pub fn unpack_all(&mut self) -> Result<()> {
         loop {
             let mut header_bytes = [0u8; HEADER_LEN];
-            let read_count = match self.file.read(&mut header_bytes) {
+            let read_count = match pack_file.read(&mut header_bytes) {
                 Ok(v) => v,
                 Err(e) => return Err(ErrorKind::IO(e)),
             };
             if read_count != header_bytes.len() {
                 break Ok(());
-            }
+            };
 
-            println!("header bytes: {:x?}", header_bytes);
-
-            println!("make header");
             let header = match FileHeader::from_bytes(&header_bytes) {
                 Ok(v) => v,
                 Err(e) => return Err(e),
             };
 
-            println!("read path bytes");
             let mut path_bytes = vec![0u8; header.path_len_usize()?];
-            if let Err(e) = self.file.read_exact(&mut path_bytes) {
+            if let Err(e) = pack_file.read_exact(&mut path_bytes) {
                 return Err(ErrorKind::IO(e));
             };
 
-            println!("make path");
-            let path = match std::str::from_utf8(&path_bytes) {
+            // Creates file path
+            let path = PathBuf::from(match self.0.file_stem() {
+                Some(v) => v,
+                None => return Err(ErrorKind::PathError),
+            })
+            .join(PathBuf::from(match std::str::from_utf8(&path_bytes) {
                 Ok(v) => v,
                 Err(_) => return Err(ErrorKind::Utf8Error),
-            };
+            }));
 
-            let parent_dir_path = match PathBuf::from(path).parent() {
+            let parent_dir_path = match PathBuf::from(&path).parent() {
                 Some(v) => v.to_path_buf(),
                 None => return Err(ErrorKind::PathError),
             };
@@ -173,7 +146,6 @@ impl PackReader {
                 Err(e) => return Err(ErrorKind::IO(e)),
             };
 
-            println!("iter and rem");
             let iterations =
                 header
                     .file_len_u64()
@@ -188,10 +160,9 @@ impl PackReader {
                     Err(_) => return Err(ErrorKind::TryFromIntError),
                 });
 
-            println!("buffer1");
             let mut buffer = [0u8; DEFAULT_BUFFER_LEN];
             for _ in 0..iterations {
-                match self.file.read_exact(&mut buffer) {
+                match pack_file.read_exact(&mut buffer) {
                     Ok(v) => v,
                     Err(e) => return Err(ErrorKind::IO(e)),
                 };
@@ -200,7 +171,6 @@ impl PackReader {
                 };
             }
 
-            println!("buffer2");
             let mut buffer = vec![
                 0u8;
                 match rem.try_into() {
@@ -208,7 +178,7 @@ impl PackReader {
                     Err(_) => return Err(ErrorKind::TryFromIntError),
                 }
             ];
-            match self.file.read_exact(&mut buffer) {
+            match pack_file.read_exact(&mut buffer) {
                 Ok(v) => v,
                 Err(e) => return Err(ErrorKind::IO(e)),
             };
@@ -217,39 +187,53 @@ impl PackReader {
             };
         }
     }
+
+    pub fn path(&self) -> &PathBuf {
+        return &self.0;
+    }
 }
 
 /// Manage pack file headers.
 ///
 /// A pack file header is made up of 12 bytes:
 /// - 4 bytes representing the length of the associated path
-/// - 8 bytes representing the length of the associated file.
+/// - 8 bytes representing the length of the associated file
 ///
 /// The maximum file length that can be stored in the
 /// header is about 18.4 Terabytes.
 pub struct FileHeader {
+    path: Option<PathBuf>,
     path_len: u32,
     file_len: u64,
 }
 
 impl FileHeader {
     /// Creates a pack file header.
-    pub fn new(path: &str) -> Result<FileHeader> {
+    pub fn new(file_path: &PathBuf, dir_path: &PathBuf) -> Result<FileHeader> {
+        let path_str = match file_path.strip_prefix(dir_path) {
+            Ok(v) => match v.to_str() {
+                Some(v) => v,
+                None => return Err(ErrorKind::PathError),
+            },
+            Err(_) => return Err(ErrorKind::PathError),
+        };
+
         // The length of the path (as bytes).
-        let path_len = match u32::try_from(path.as_bytes().len()) {
+        let path_len = match u32::try_from(path_str.as_bytes().len()) {
             Ok(v) => v,
             Err(_) => return Err(ErrorKind::TryFromIntError),
         };
         // The length of the file.
-        let file_len = match fs::metadata(path) {
+        let file_len = match fs::metadata(file_path) {
             Ok(v) => v.len(),
             Err(e) => return Err(ErrorKind::IO(e)),
         };
 
-        println!("path: {:x?} ({})", path_len.to_be_bytes(), path.len());
-        println!("file: {:x?} ({})", file_len.to_be_bytes(), file_len);
-
-        Ok(FileHeader { path_len, file_len })
+        Ok(FileHeader {
+            path: Some(PathBuf::from(path_str)),
+            path_len,
+            file_len,
+        })
     }
 
     /// Extracts a file header from an array of 12 bytes.
@@ -264,6 +248,7 @@ impl FileHeader {
         };
 
         Ok(FileHeader {
+            path: None,
             path_len: match u32::from_be_bytes(path_len_bytes).try_into() {
                 Ok(v) => v,
                 Err(_) => return Err(ErrorKind::TryFromIntError),
@@ -277,6 +262,13 @@ impl FileHeader {
         Ok([
             self.path_len.to_be_bytes().as_slice(),
             self.file_len.to_be_bytes().as_slice(),
+            match &self.path {
+                Some(v) => match v.to_str() {
+                    Some(v) => v.as_bytes(),
+                    None => return Err(ErrorKind::PathError),
+                },
+                None => return Err(ErrorKind::PathError),
+            },
         ]
         .concat())
     }
