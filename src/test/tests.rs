b391abdb2004f054b918b60f4b9c6ebed3eab0b5
valflrt <valflrt@pm.me>
2022-11-09 17:08:47 +0100

    Edit tests
    - Add test for WalkDir

--- src/test/tests.rs
+++ src/test/tests.rs
@@ -1,5 +1,6 @@
-use crate::crypto::Crypto;
-use crate::tmp_dir::TmpDir;
+use std::path::PathBuf;
+
+use crate::{crypto::Crypto, tmp_dir::TmpDir, walk_dir::WalkDir};
 
 // Crypto tests
 
@@ -72,3 +73,27 @@ fn fail_to_decrypt_file_when_key_is_wrong() {
 }
 
 // WalkDir tests
+
+#[test]
+fn walk_directory_and_encounter_every_file_in_it() {
+    let tmp_dir = TmpDir::new().unwrap();
+
+    tmp_dir.write_file("a", &[]).unwrap();
+    tmp_dir.write_file("b", &[]).unwrap();
+    tmp_dir.create_dir("c").unwrap();
+    tmp_dir.write_file("c/a", &[]).unwrap();
+    tmp_dir.write_file("c/b", &[]).unwrap();
+    tmp_dir.write_file("d", &[]).unwrap();
+
+    let walk_dir = WalkDir::new(tmp_dir.path()).unwrap();
+
+    let entries: Vec<PathBuf> = walk_dir.map(|e| e.unwrap().path()).collect();
+
+    assert!(entries
+        .iter()
+        .all(|e| vec!["a", "b", "c", "c/a", "c/b", "d"]
+            .iter()
+            .map(|p| tmp_dir.path().join(p))
+            .find(|p| e == p)
+            .is_some()));
+}
